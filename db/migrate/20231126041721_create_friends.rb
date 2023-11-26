827361ba6c88729dbf60c6228532901c3bf0a388
shubham <shubham.bhoyar077@gmail.com>
2023-11-26 09:51:32 +0530

    Fix typo and migrate

--- db/migrate/20231126041721_create_friends.rb
+++ db/migrate/20231126041721_create_friends.rb
@@ -1,8 +1,8 @@
 class CreateFriends < ActiveRecord::Migration[6.1]
   def change
     create_table :friends do |t|
-      t.reference :user
-      t.integer :friend_id
+      t.references :user, null: false, foreign_key: true
+      t.integer :friend_id, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_06_083940) do
+ActiveRecord::Schema.define(version: 2023_11_26_041721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "friends", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "friend_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_friends_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +37,5 @@ ActiveRecord::Schema.define(version: 2021_10_06_083940) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friends", "users"
 end
