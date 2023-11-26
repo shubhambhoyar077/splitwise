7429bb11557befca695e550ff5aabba936146043
shubham <shubham.bhoyar077@gmail.com>
2023-11-26 10:11:36 +0530

    Add func for get all friend and that are not friend

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
  has_many :friends, foreign_key: 'friend_id'
+
+ def all_friends
+  friend_i_added = Friend.where(user_id: id).pluck(:friend_id)
+  friend_they_added_me = Friend.where(friend_id: id).pluck(:user_id)
+  ids = friend_i_added + friend_they_added_me
+  User.where(id: ids)
+end
+
+def not_friends
+  User.where.not(id: id) - all_friends
+end
 end
