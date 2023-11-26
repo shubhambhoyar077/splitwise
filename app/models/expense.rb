fd2e7d43055bfcdb72a2267feec0f5faabcc806c
shubham <shubham.bhoyar077@gmail.com>
2023-11-26 17:33:25 +0530

    Add nested attribute for expense slpit

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,7 @@
 class Expense < ApplicationRecord
   belongs_to :payer, class_name: "User"
   has_many :expense_splits
+  accepts_nested_attributes_for :expense_splits, reject_if: :all_blank, allow_destroy: true
 
   validates :payer_id, presence: true
   validates :total_amount, presence: true, numericality: { greater_than: 0 }
