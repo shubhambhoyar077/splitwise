324da9255b103eee706e23decd52f6df7d82cc80
shubham <shubham.bhoyar077@gmail.com>
2023-11-26 17:36:19 +0530

    Create expense controller with create func

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,17 @@
+class ExpensesController < ApplicationController
+
+  def create
+    @expense = Expense.new(expense_params)
+    if @expense.save
+      redirect_to root_path, notice: 'Expense added successfully'
+    else
+      redirect_to root_path, notice: 'Expense not added'
+    end
+  end
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:date, :description, :total_amount, :payer_id, expense_splits_attributes: [:id, :recipient_id, :amount, :_destroy])
+  end
+end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,8 @@ class StaticController < ApplicationController
   def dashboard
     @friends = current_user.all_friends
     @not_friends = current_user.not_friends
+    @expense = Expense.new
+    @expense.expense_splits.new
   end
 
   def person
