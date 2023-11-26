8dc089e00401d86f1c9b5187225725e6f5523585
shubham <shubham.bhoyar077@gmail.com>
2023-11-26 10:07:43 +0530

    Add association and validation for friends

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,2 +1,5 @@
 class Friend < ApplicationRecord
+  belongs_to :user
+  validates :user_id, uniqueness: { scope: :friend_id }
+  validates :friend_id, uniqueness: { scope: :user_id }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+ has_many :friends, foreign_key: 'friend_id'
 end
