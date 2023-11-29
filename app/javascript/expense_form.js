42e77a5d6c3ab750f61f3092ecc78443d7b33567
shubham <shubham.bhoyar077@gmail.com>
2023-11-29 17:42:59 +0530

    Refactor code

--- app/javascript/expense_form.js
+++ app/javascript/expense_form.js
@@ -2,41 +2,39 @@ document.addEventListener('turbolinks:load', function () {
   function expenseForm() {
     let equalCheckbox = document.getElementById('equal-checkbox');
     let fixedCheckbox = document.getElementById('fixed-checkbox');
+    var totalAmountInput = document.getElementById('total-amount');
+    let totalAmountValue = 0;
+
+    equalCheckbox.checked = true;
+
+    function updateDynamicAmountInputs() {
+      let dynamicAmountInputs = document.querySelectorAll('.split-amount');
+      dynamicAmountInputs.forEach(function (dynamicAmountInput) {
+        if (equalCheckbox.checked) {
+          dynamicAmountInput.setAttribute('readonly', true);
+          dynamicAmountInput.value =
+            totalAmountValue / dynamicAmountInputs.length;
+        } else {
+          dynamicAmountInput.removeAttribute('readonly');
+        }
+      });
+    }
 
     equalCheckbox.addEventListener('change', function () {
       fixedCheckbox.checked = !equalCheckbox.checked;
-      var dynamicAmountInputs = document.querySelectorAll('.split-amount');
-      dynamicAmountInputs.forEach(function (dynamicAmountInput) {
-        dynamicAmountInput.readonly = true;
-      });
+      updateDynamicAmountInputs();
     });
 
     fixedCheckbox.addEventListener('change', function () {
       equalCheckbox.checked = !fixedCheckbox.checked;
-      var dynamicAmountInputs = document.querySelectorAll('.split-amount');
-      dynamicAmountInputs.forEach(function (dynamicAmountInput) {
-        dynamicAmountInput.readonly = false;
-      });
+      updateDynamicAmountInputs();
     });
-
-    var totalAmountInput = document.getElementById('total-amount');
-    let totalAmountValue = 0;
 
     totalAmountInput.addEventListener('input', function () {
-      // Read the total_amount value when it changes
-      totalAmountValue = parseFloat(totalAmountInput.value);
+      totalAmountValue = parseFloat(totalAmountInput.value) || 0;
       console.log('Total Amount changed to:', totalAmountValue);
-      var dynamicAmountInputs = document.querySelectorAll('.split-amount');
-      if (isNaN(totalAmountValue)) {
-        totalAmountValue = 0;
-      }
-
-      dynamicAmountInputs.forEach(function (dynamicAmountInput) {
-        dynamicAmountInput.value =
-          totalAmountValue / dynamicAmountInputs.length;
-      });
+      updateDynamicAmountInputs();
     });
   }
-  console.log('working');
   expenseForm();
 });
